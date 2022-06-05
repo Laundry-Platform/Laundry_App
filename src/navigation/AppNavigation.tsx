f857a5546d3f097bb83a8b66725ed0f69e9c4c69
daksae98 <hskim0610@gmail.com>
2022-06-05 23:39:43 +0900

    Add: Login Page
    로그인 페이지 추자

--- src/navigation/AppNavigation.tsx
+++ src/navigation/AppNavigation.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import BottomTabNavigation from './BottomTabNavigation';
+import AuthNavigation from './AuthNavigation';
 import type { NativeStackNavigationProp } from '@react-navigation/native-stack';
 
 type AppNavigationStackParamList = {
   BottomTab: undefined;
+  Auth: undefined;
 };
 
 export type AppNavigationStackProps = NativeStackNavigationProp<AppNavigationStackParamList>;
@@ -14,6 +16,7 @@ const Stack = createNativeStackNavigator<AppNavigationStackParamList>();
 const AppNavigation: React.FC = () => {
   return (
     <Stack.Navigator>
+      <Stack.Screen name="Auth" component={AuthNavigation} options={{ headerShown: false }} />
       <Stack.Screen
         name="BottomTab"
         component={BottomTabNavigation}
---
+++ src/screens/Auth/Login.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import LoginView from './LoginView';
+
+interface LoginProps {}
+
+const Login: React.FC<LoginProps> = () => {
+  return <LoginView />;
+};
+
+export default Login;
